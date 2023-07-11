ecbc7798362caee2855794417d5449d28a443a74
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-11 16:29:54 +0300

    update the calander UI

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,38 +4,30 @@ import './Calculator.css';
 function Calculator() {
   return (
     <div className="calculator">
-      <div className="display">0</div>
+      <div className="display"><span>0</span></div>
       <div className="buttons">
-        <div className="row">
-          <button className="button" type="button">AC</button>
-          <button className="button" type="button">+/-</button>
-          <button className="button" type="button">%</button>
-          <button className="button" type="button">/</button>
-        </div>
-        <div className="row">
-          <button className="button" type="button">7</button>
-          <button className="button" type="button">8</button>
-          <button className="button" type="button">9</button>
-          <button className="button" type="button">*</button>
-        </div>
-        <div className="row">
-          <button className="button" type="button">4</button>
-          <button className="button" type="button">5</button>
-          <button className="button" type="button">6</button>
-          <button className="button" type="button">-</button>
-        </div>
-        <div className="row">
-          <button className="button" type="button">1</button>
-          <button className="button" type="button">2</button>
-          <button className="button" type="button">3</button>
-          <button className="button" type="button">+</button>
-        </div>
-        <div className="row">
-          <button className="button" type="button">0</button>
-          <button className="button" type="button">.</button>
-          <button className="button" type="button">=</button>
-        </div>
+        <button className="button row" type="button">AC</button>
+        <button className="button row" type="button">+/-</button>
+        <button className="button row" type="button">%</button>
+        <button className="button row orange" type="button">/</button>
+        <button className="button row" type="button">7</button>
+        <button className="button row" type="button">8</button>
+        <button className="button row" type="button">9</button>
+
+        <button className="button row orange" type="button">*</button>
+        <button className="button row" type="button">4</button>
+        <button className="button row" type="button">5</button>
+        <button className="button row" type="button">6</button>
+        <button className="button row orange" type="button">-</button>
+        <button className="button row" type="button">1</button>
+        <button className="button row" type="button">2</button>
+        <button className="button row" type="button">3</button>
+        <button className="button row orange" type="button">+</button>
+        <button className="button row two" type="button">0</button>
+        <button className="button row" type="button">.</button>
+        <button className="button row orange" type="button">=</button>
       </div>
+
     </div>
   );
 }
