0637e1ee8ea107cb11f6ed67d7ec84b3d8e64445
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-11 18:01:03 +0300

    created another component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import './Calculator.css';
+import CalculatorTitle from './CalculatorTitle';
 
 function Calculator() {
   return (
-    <div className="calculator">
-      <div className="display"><span>0</span></div>
-      <div className="buttons">
-        <button className="button row" type="button">AC</button>
-        <button className="button row" type="button">+/-</button>
-        <button className="button row" type="button">%</button>
-        <button className="button row orange" type="button">/</button>
-        <button className="button row" type="button">7</button>
-        <button className="button row" type="button">8</button>
-        <button className="button row" type="button">9</button>
+    <div className="container">
+      <CalculatorTitle />
 
-        <button className="button row orange" type="button">*</button>
-        <button className="button row" type="button">4</button>
-        <button className="button row" type="button">5</button>
-        <button className="button row" type="button">6</button>
-        <button className="button row orange" type="button">-</button>
-        <button className="button row" type="button">1</button>
-        <button className="button row" type="button">2</button>
-        <button className="button row" type="button">3</button>
-        <button className="button row orange" type="button">+</button>
-        <button className="button row two" type="button">0</button>
-        <button className="button row" type="button">.</button>
-        <button className="button row orange" type="button">=</button>
+      <div className="calculator">
+        <div className="display"><span>0</span></div>
+        <div className="buttons">
+          <button className="button row" type="button">AC</button>
+          <button className="button row" type="button">+/-</button>
+          <button className="button row" type="button">%</button>
+          <button className="button row orange" type="button">/</button>
+          <button className="button row" type="button">7</button>
+          <button className="button row" type="button">8</button>
+          <button className="button row" type="button">9</button>
+
+          <button className="button row orange" type="button">*</button>
+          <button className="button row" type="button">4</button>
+          <button className="button row" type="button">5</button>
+          <button className="button row" type="button">6</button>
+          <button className="button row orange" type="button">-</button>
+          <button className="button row" type="button">1</button>
+          <button className="button row" type="button">2</button>
+          <button className="button row" type="button">3</button>
+          <button className="button row orange" type="button">+</button>
+          <button className="button row two" type="button">0</button>
+          <button className="button row" type="button">.</button>
+          <button className="button row orange" type="button">=</button>
+        </div>
+
       </div>
-
     </div>
   );
 }
---
+++ src/components/CalculatorTitle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Calculator.css';
+
+export default function CalculatorTitle() {
+  return (
+    <div className="title">Simple Calculator | React</div>
+  );
+}
