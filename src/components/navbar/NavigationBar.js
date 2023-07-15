41ccd1f7e60c3a4671480f8502d17d6367bd43af
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-15 08:45:45 +0300

    changed from normal navigation to react router

--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import './navbar.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function NavigationBar() {
   return (
-    <nav>
-      <div className="logo">
-        <span>Math Magicians</span>
-      </div>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/calculator">Calculator</a>
-        </li>
-        <li>
-          <a href="/qoute">Qoute</a>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav>
+        <div className="logo">
+          <span>Math Magicians</span>
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/qoute">Qoute</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path='/' Component={} />
+        <Route exact path='/' Component={} />
+        <Route exact path='/' Component={} />
+      </Switch>
+    </Router>
   );
 }
 
