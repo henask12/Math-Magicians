56d3351acb2b06e1c99f2e7ded52cba268feb9f1
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-15 10:44:28 +0300

    Fix Navbar with css

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Calculator from './components/Calculator';
+import NavigationBar from './components/navbar/NavigationBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavigationBar />
     <Calculator />
   </React.StrictMode>,
 );
