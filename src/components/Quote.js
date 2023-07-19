12b67964fddcb213e743af5735b3a4d18bed6b2e
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-19 10:37:49 +0300

    adjusted the UI qoute

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,8 +3,11 @@ import QuoteDisplay from './QouteDisplay';
 
 function Quote() {
   const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [datas, setDatas] = useState();
 
   const fetchQuote = async () => {
     setIsLoading(true);
@@ -18,12 +21,23 @@ function Quote() {
         },
       );
 
+      const response2 = await fetch(
+        'https://api.api-ninjas.com/v1/quotes?category=life',
+        {
+          headers: { 'X-Api-Key': 'KngqeNNPDXdnVHYdSkldtQ==mlFR9OH13F8KvuPk' },
+        },
+      );
+
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
       const data = await response.json();
+      const data2 = await response2.json();
       setQuote(data[0]?.quote);
+      setAuthor(data[0]?.author);
+      setCategory(data[0]?.category);
+      setDatas(data2);
     } catch (error) {
       setError(error.message);
     } finally {
@@ -36,19 +50,27 @@ function Quote() {
   }, []);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div role="status" className="flex items-center justify-center mt-10 mb-10">
+        <svg aria-hidden="true" className="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
   }
 
   if (error) {
     return (
-      <div>
+      <div className="bg-red-200 p-4 rounded-lg shadow-md">
         Error:
         {error}
       </div>
     );
   }
 
-  return <QuoteDisplay quote={quote} />;
+  return <QuoteDisplay quote={quote} author={author} category={category} quote2={datas} />;
 }
 
 export default Quote;
