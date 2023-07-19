6a7615feddbf8d8f5bd0e9de9c7f516abd13eb89
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-19 10:00:48 +0300

    made sure it is navigating

--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import './navbar.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+import Homepage from '../home';
+import Calculator from '../Calculator';
+import Quote from '../Quote';
 
 function NavigationBar() {
   return (
     <Router>
-      <nav>
-        <div className="logo">
-          <span>Math Magicians</span>
+      <nav className="bg-gray-800 py-4">
+        <div className="container mx-auto px-4 flex items-center justify-between">
+          <div className="logo">
+            <span className="text-white font-bold text-xl">Math Magicians</span>
+          </div>
+          <ul className="flex space-x-4 text-white font-bold">
+            <li>
+              <Link to="/" className="text-white hover:text-teal-200">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/calculator" className="text-white hover:text-teal-200">
+                Calculator
+              </Link>
+            </li>
+            <li>
+              <Link to="/quote" className="text-white hover:text-teal-200">
+                Quote
+              </Link>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/calculator">Calculator</Link>
-          </li>
-          <li>
-            <Link to="/qoute">Qoute</Link>
-          </li>
-        </ul>
       </nav>
-      <Switch>
-        <Route exact path='/' Component={} />
-        <Route exact path='/' Component={} />
-        <Route exact path='/' Component={} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/calculator" element={<Calculator />} />
+        <Route exact path="/quote" element={<Quote />} />
+      </Routes>
     </Router>
   );
 }
